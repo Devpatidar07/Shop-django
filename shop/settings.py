79424378497c0226989815ac8f61c158c4dd3b56
Devendra Patidar <karishmapatidar2154@gmail.com>
2025-02-16 17:28:01 +0530

    django authentication

--- shop/settings.py
+++ shop/settings.py
@@ -135,3 +135,22 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True 
+EMAIL_HOST_USER = 'teamdevendrapatidar@gmail.com'
+EMAIL_HOST_PASSWORD = 'sqjgvwqvuzowdhjp'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
