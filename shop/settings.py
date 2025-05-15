0d14003b8153ce7e1a60420c5e92d6dfea820b98
Devendra Patidar <devendrapatidar126@gmail.com>
2025-05-15 21:25:14 +0530

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@*87z3_$l=y@cssoc9&$gr!0b7jpc^8(0(!q(1p&c=%m(0yno0'
+SECRET_KEY = '#secret key used in production secret!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
